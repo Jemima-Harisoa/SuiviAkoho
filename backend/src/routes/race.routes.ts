22bf82f314c4930529c313897ff922a7380205ad
Jemima-Harisoa <jemimarazafimandimby@gmail.com>
2026-03-11 11:24:59 +0300

    refact : routes

--- backend/src/routes/race.routes.ts
+++ backend/src/routes/race.routes.ts
@@ -6,7 +6,5 @@ const router = Router();
 router.get('/', raceController.getAllRaces);
 router.get('/:id', raceController.getRaceById);
 router.post('/create', raceController.createRace);
-router.get('/create'));
-
 
 export default router;
